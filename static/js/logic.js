684a6dc97408f7f7744e31583372bb9ee158eff4
juliedundas <juliedundas@Julies-MacBook-Pro-2.local>
2020-04-03 17:32:35 -0700

    map coordinates loaded

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,1 +1,25 @@
-//Code here:
+// Creating map object
+var map = L.map("map", {
+    center: [53.67706, - 1.629582],
+    zoom: 3
+});
+
+// Adding tile layer
+L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.streets",
+    accessToken: API_KEY
+}).addTo(map);
+
+// Link in the geojson data
+var link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+// Grabbing our GeoJSON data..
+d3.json(link, function (data) {
+    // Creating a GeoJSON layer with the retrieved data
+    console.log(data);
+
+
+    L.geoJson(data).addTo(map);
+});
