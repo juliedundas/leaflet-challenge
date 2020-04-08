d13663bf57c0bd8c559b1b07247f779694d92833
juliedundas <juliedundas@Julies-MacBook-Pro-2.local>
2020-04-07 17:22:41 -0700

    updating logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -5,21 +5,42 @@ var map = L.map("map", {
 });
 
 // Adding tile layer
-L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+var satelliteMap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
-    id: "mapbox.streets",
+    id: "mapbox.satellite",
     accessToken: API_KEY
 }).addTo(map);
 
 // Link in the geojson data
-var link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+//var link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
-// Grabbing our GeoJSON data..
-d3.json(link, function (data) {
-    // Creating a GeoJSON layer with the retrieved data
-    console.log(data);
+function createMarkers(response) {
+
+    // Pull the "stations" property off of response.data
+    var earthquakes = response.data.features;
+
+    // Initialize an array to hold bike markers
+    var earthquakeMarkers = [];
+
+    // Loop through the stations array
+    for (var i = 0; i < earthquakes.length; i++) {
+        var earthquake = earthquakes[i];
+
+        // For each station, create a marker and bind a popup with the station's name
+        var earthquakeMarker = L.marker([features.coordinates[0], features.coordinates[1]])
+            .bindPopup("<h3>" + features.properties + "<h3><h3>Capacity: " + features.detail + "<h3>");
+
+        // Add the marker to the bikeMarkers array
+        bikeMarkers.push(earthquakeMarker);
+    }
 
 
-    L.geoJson(data).addTo(map);
-});
+
+
+
+
+
+
+
+
