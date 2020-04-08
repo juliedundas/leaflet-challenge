83ce9a40348f1b02fbd9caa95c1a04320cf64299
juliedundas <juliedundas@Julies-MacBook-Pro-2.local>
2020-04-07 23:32:19 -0700

    step 1 complete

--- static/js/logic.js
+++ static/js/logic.js
@@ -25,29 +25,31 @@ d3.json(link, function (data) {
         var magnitude = feature.properties.mag
         //console.log(magnitude)
         return {
-            opacity: 0.75,
+            opacity: 1,
             fillOpactiy: 1,
+            fillColor: assignColor(magnitude),
             color: assignColor(magnitude),
             //stroke: false,
-            fillColor: assignColor(magnitude),
             // Adjust radius
             radius: assignRadius(magnitude),
-            weight: 0.9
+            weight: 1
         }
     }
 
     //Create the color function to style the different data points
     function assignColor(magnitude) {
         if (magnitude > 5) {
-            return "black";
+            return "maroon";
         } else if (magnitude > 4) {
             return "red"
         } else if (magnitude > 3) {
             return "orange"
         } else if (magnitude > 2) {
             return "yellow"
+        } else if (magnitude > 1) {
+            return "darkgreen"
         } else
-            return "green"
+            return "lightgreen"
     }
 
     //Create the radius function to style the sizes of the data points
@@ -65,12 +67,43 @@ d3.json(link, function (data) {
         },
         style: createMarkers,
         onEachFeature: function (feature, layer) {
-            layer.bindPopup("Magniture " + feature.properties.mag)
+            //display magnitue and description for earthquake on click
+            layer.bindPopup("Magnitude " + feature.properties.mag + "<br> Description: " + feature.properties.title)
         }
-
+        //add markers to map
     }).addTo(map);
 
+
+    //create a variable for the legend
+    var legend = L.control({
+        position: "bottomright"
+    });
+
+    //add legend details
+
+    legend.onAdd = function (map) {
+        var div = L.DomUtil.create("div", "legend");
+
+        var scale = [0, 1, 2, 3, 4, 5];
+
+        var colors = ["black", "red", "orange", "yellow", "green"];
+
+        //div.innerHTML += 'Magnitude<br>'
+
+        //Loop through and assign the correct color to the legend
+
+        for (var i = 0; i < scale.length; i++) {
+            div.innerHTML +=
+                "<i style='background: " + assignColor(scale[i] + 1) + "'>&nbsp&nbsp&nbsp&nbsp</i> " +
+                scale[i] + (scale[i + 1] ? "&ndash;" + scale[i + 1] + "<br>" : "+");
+
+        }
+        return div
+    };
+
+    legend.addTo(map)
 })
+
 
 
 
